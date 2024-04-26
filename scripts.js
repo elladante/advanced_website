009f01a1118d0bad1d23cc80acb1ec7350d44407
elladante <dante8346@saskpolytech.ca>
2024-04-26 02:27:17 -0600

    This is my third commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+    var errorMessage = "";
+});
